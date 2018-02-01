c563ce7778e6be8572cf829a12871a4f36c55b06
Steve Berczuk <steve@berczuk.com>
2018-02-01 14:27:46 -0500

    remove template function greeting()

--- tetrascience/src/main/java/App.java
+++ tetrascience/src/main/java/App.java
@@ -50,9 +50,6 @@ public class App {
         }
     }
 
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
         App app = new App();
